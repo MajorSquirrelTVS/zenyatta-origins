e2c334e55f97449f5dce961d5aa05b9504aeac85
airchi <dav56.even@gmail.com>
2017-03-28 13:19:14 +0900

    jump implemented

--- Assets/Script/ZenattyaController.cs
+++ Assets/Script/ZenattyaController.cs
@@ -4,27 +4,51 @@ using UnityEngine;
 
 public class ZenattyaController : MonoBehaviour {
 
-    public float MaxSpeed = 10.0f;
+    public float maxSpeed = 10.0f;
     bool facingRight = true;
     bool grounded = true;
     public Transform groundCheck;
-	// Use this for initialization
-	void Start () {
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+    public float jumpForce = 700.0f;
+    Rigidbody2D rigidbody;
 
-	}
+    // Use this for initialization
+    void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+
+        /** JUMP **/
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+        /** JUMP **/
+
+
+        /** MOVEMENT **/
+
         float move = Input.GetAxis("Horizontal");
 
-        Rigidbody2D rigidbody= GetComponent<Rigidbody2D>();
-        rigidbody.velocity = new Vector2(move * MaxSpeed, rigidbody.velocity.y);
+        rigidbody.velocity = new Vector2(move * maxSpeed, rigidbody.velocity.y);
 
         if (move > 0 && !facingRight)
             Flip();
         else if (move < 0 && facingRight)
             Flip();
+
+        /** MOVEMENT **/
 	}
+
+    void Update()
+    {
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(new Vector2(0, jumpForce));
+        }
+    }
 
     void Flip()
     {
