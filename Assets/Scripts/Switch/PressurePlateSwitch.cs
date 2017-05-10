8ed6027081eac0fb525ecd66358ebadd28b75982
airchi <dav56.even@gmail.com>
2017-05-10 15:43:54 +0900

    pressure plate works

---
+++ Assets/Scripts/Switch/PressurePlateSwitch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlateSwitch : MonoBehaviour {
+
+
+    public GameObject connect_object;
+    public bool is_on;
+	// Use this for initialization
+	void Start () {
+        is_on = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(is_on);
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject == connect_object)
+        {
+            is_on = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject == connect_object)
+        {
+            is_on = false;
+        }
+    }
+}
