2a314f3cb7d1c360e2f973154c72f54887cb6746
airchi <dav56.even@gmail.com>
2017-05-11 04:01:30 +0900

    prefab and editing level 1 1st step

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+
+    public Text restartText;
+    public Text finishText;
+
+    bool restart;
+    bool finish;
+    // Use this for initialization
+    void Start () {
+        restart = false;
+        finish = false;
+        restartText.text = "";
+        finishText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (restart)
+            restartText.text = "Press R for restart";
+        else
+            restartText.text = "";
+
+        if (finish)
+            finishText.text = "Finish !";
+        else
+            finishText.text = "";
+    }
+
+    void setRestart(bool value)
+    {
+        restart = value;
+    }
+
+    void setFinish(bool value)
+    {
+        finish = value;
+    }
+}
