3328d0433ec607d749917837c0f4b5118f2b8669
airchi <dav56.even@gmail.com>
2017-06-15 03:15:20 +0900

    cinematic

---
+++ Assets/Scripts/PlayMovie.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))] 
+
+public class PlayMovie : MonoBehaviour {
+
+    public MovieTexture movie;
+    private AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        movie.Play();
+        source.Play();
+
+
+    }
+
+    void Update()
+    {
+        if (!movie.isPlaying)
+        {
+            Debug.Log("movie end");
+            transform.gameObject.SetActive(false);
+        }
+
+    }
+}
